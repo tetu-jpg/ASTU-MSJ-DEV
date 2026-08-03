6fb3d812fd2dc3ac53c41e2f06959a728f6a3ddc
Tesnim Abdella <tesnimabdella52@gmail.com>
2026-08-03 07:41:33 +0300

    first

---
+++ task manager/backend/data/taskData.js
@@ -0,0 +1,28 @@
+let tasks =[
+    {
+        id:1,
+        title:"Finish Lecture 2 homework",
+        completed:false,
+        priority:"high"
+    },
+    {
+        id:2,
+        title:"Review express routing",
+        completed:true,
+        priority:"medium"
+    },
+    {
+        id:3,
+        title:"Set up env files",
+        completed:false,
+        priority:"low"
+    },
+    {
+        id:4,
+        title:"Push project to Github",
+        completed:false,
+        priority:"high"
+    }
+    
+];
+module.exports = tasks; 
